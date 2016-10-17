f8e5d6b387ca54557ba765dbbd8efb2960261103
Ishinagi_moeta <jackktx@gmail.com>
2016-10-17 14:48:36 +0800

    分类界面的数据载入，需要Gson 216101703

--- app/src/main/java/com/zzh/dell/guoku/fragment/CategoryFragment.java
+++ app/src/main/java/com/zzh/dell/guoku/fragment/CategoryFragment.java
@@ -3,19 +3,41 @@ package com.zzh.dell.guoku.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.zzh.dell.guoku.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import cn.trinea.android.view.autoscrollviewpager.AutoScrollViewPager;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CategoryFragment extends Fragment {
 
+    @BindView(R.id.category_view_pager)
+    AutoScrollViewPager autoScrollViewPager;
+
+    @BindView(R.id.category_user_pager)
+    ViewPager userPager;
+
+    @BindView(R.id.category_category_pager)
+    ViewPager categoryPager;
+
+    @BindView(R.id.category_entity_list)
+    ListView entityList;
+
+    @BindView(R.id.category_imagetext_list)
+    ListView imagetextList;
+
+    @BindView(R.id.category_search_view)
+    SearchView searchView;
 
     public CategoryFragment() {
         // Required empty public constructor
